9a19e2304c87e4676d0c5c8de59ee55be4beb3a8
Audris Mockus <audris@utk.edu>
2014-09-28 09:47:33 -0400

    Update RepoSize.py

--- RepoSize.py
+++ RepoSize.py
@@ -11,12 +11,12 @@ while True:
   t = r.text
   jsonDict = json.loads (t)
   for myIterator in jsonDict['values']:
-    (scm, name, size,has_issues,created,updated) = ('', '', '', '', '', '') 
+    (scm, name, size, has_issues, created, updated) = ('', '', '', '', '', '') 
     for key, value in myIterator.iteritems(): 
       if key == 'scm':
         scm = value
       if key == 'size':
-        size = value
+        size = str(value)
       if key == 'full_name':
         name = value
       if key == 'created_on':
@@ -24,10 +24,7 @@ while True:
       if key == 'updated_on':
         updated = value
       if key == 'has_issues':
-        has_issues = value
+        has_issues = str(value)
     print size+';'+scm+';'+has_issues+';'+created+';'+updated+';'+name
   if 'next' not in jsonDict: break
   else: url = jsonDict['next']
-
-for key, value in tracker.iteritems(): 
-  print(key+': '+str(value))
