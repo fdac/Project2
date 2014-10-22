27bed46aaf7dd61b03ced45a6366d4a71c2d8878
Audris Mockus <audris@mockus.org>
2014-10-22 17:01:46 -0400

    Expaned getUsers.sh

--- getUsers.sh
+++ getUsers.sh
@@ -3,6 +3,13 @@ python watchersToUsers.py | sort -u > wch.u
 python followersToUsers.py | sort -u > fw.u
 python followingToUsers.py | sort -u > fwg.u
 python eventsToUsers.py | sort -u > evt.u 
+
+cut -d/ -f7 fw.out  | sort -u > fw.done
+cut -d/ -f7 fwg.out  | sort -u > fwg.done
+
 cat fw.done wch.u fw.u fwg.u evt.u | sort -u > all.u
 join -v1 -t\; all.u fw.done > followers.todo
-python gatherFw.py >> fw.out 2>> fw.err &
+join -v1 -t\; all.u fwg.done > following.todo
+python gatherFw.py >> fw.out 2>> fw.err 
+python gatherFwg.py >> fwg.out 2>> fwg.err 
+
